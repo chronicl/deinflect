8de012e4677ae5945ee866bd060b9968861904cd
chronicl <lennard.koehler@gmx.net>
2023-11-08 00:50:43 +0100

    fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
-///! ### Fast japanese deinflection.
-///!
-///! ```rust
-///! use deinflect::Deinflections;
-///!
-///! fn main() {
-///!     let deinflections = Deinflections::from_word("聞かれました");
-///!
-///!     // iterate over all possible deinflections
-///!     for deinflection in deinflections.iter() {
-///!         // get the deinflected word as a string
-///!         let deinflected = deinflections.to_string(deinflection);
-///!         println!("{}", deinflected);
-///!     }
-///! }
-///! ```
-///!
-///! This library is based on the [yomichan japanese deinflector](https://github.com/FooSoft/yomichan)
+//! ### Fast japanese deinflection.
+//!
+//! ```rust
+//! use deinflect::Deinflections;
+//!
+//! fn main() {
+//!     let deinflections = Deinflections::from_word("聞かれました");
+//!
+//!     // iterate over all possible deinflections
+//!     for deinflection in deinflections.iter() {
+//!         // get the deinflected word as a string
+//!         let deinflected = deinflections.to_string(deinflection);
+//!         println!("{}", deinflected);
+//!     }
+//! }
+//! ```
+//!
+//! This library is based on the [yomichan japanese deinflector](https://github.com/FooSoft/yomichan)
 use bitflags::bitflags;
 use once_cell::sync::Lazy;
 use rules::INFLECTION_RULES;
